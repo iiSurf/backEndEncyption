50606c9d3b25f054c87788030e7f4f52aa10362b
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-19 11:59:13 -0700

    Back End Auth Done

--- routes/api/users.mjs
+++ routes/api/users.mjs
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { check, validationResult } from 'express-validator';
@@ -15,46 +15,75 @@ router.get('/', (req, res) => res.send('User Route'));
 // @route:   POST api/users
 // @desc:    Create User
 // @access:  Public
-
-router.get('/', [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email.').isEmail(),
-    check('password', 'Please enter a password with six or more characters.').isLength({min: 6})
-], async (req, res) => {});
-    // Check if any validation errors
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
+router.post(
+    '/',
+    [
+      check('name', 'Name is required').not().isEmpty(),
+      check('email', 'Please include a valid email').isEmail(),
+      check(
+        'password',
+        'Please enter a password with 6 or more characters'
+      ).isLength({ min: 6 }),
+    ],
+    async (req, res) => {
+      //Check if any validation errors
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
-    }
-
-    // Destructure ou req
-    const { name, email, password } = req.body
-
-    try {
-        // Check if user already exists
+      }
+  
+      //Destructure our req
+      const { name, email, password } = req.body;
+  
+      try {
+        //Check is user already exists
         let user = await User.findOne({ email });
-
+        //If they exist respond with error
         if (user) {
-            return res
-                .status(400)
-                .json({ errors: [{ msg: 'User Already Exists'}] })
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'User Already Exist' }] });
         }
-
-        // create a user
+  
+        //Create a user
         user = new User({
-            name,
-            email,
-            password,
-        })
-
-        // Encrypt password
+          name,
+          email,
+          password,
+        });
+  
+        //Encrypt password
         const salt = await bcrypt.genSalt(10);
-
+  
         user.password = await bcrypt.hash(password, salt);
-
-    } catch (err) {
-        console.error(err)
-        res.status(500).json([{ msg: "Server Error"}])
+  
+        await user.save();
+  
+        //Creating payload (data for the front end) for jwt
+        const payload = {
+          user: {
+            id: user.id,
+            name: user.name,
+          },
+        };
+  
+        jwt.sign(
+          payload,
+          process.env.jwtSecret,
+          { expiresIn: 3600 },
+          (err, token) => {
+              
+            if (err) throw err;
+  
+            res.json({ token });
+          }
+        );
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ errors: [{ msg: 'Server Error' }] });
+      }
     }
-export default router;
+  );
+
+  export default router;
